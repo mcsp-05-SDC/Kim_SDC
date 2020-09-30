f853aaa182fd9259b0e09bb1af6901b2f639ef92
john <johnferguson1440@yahoo.com>
2020-09-30 17:58:15 -0500

    added button around tabs and css to match

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,10 @@ export default class App extends Component {
           ship: "hide",
           question: "hide",
           id: 10,
-          prodInfo: null
+          prodInfo: null,
+          detailS: "selected",
+          shipS:"unselected",
+          questionS:"unselected"
 
       
       };     
@@ -23,16 +26,25 @@ export default class App extends Component {
     this.setState({detail: "show"})
     this.setState({ship: "hide"})
     this.setState({question: "hide"})
+    this.setState({detailS: "selected"})
+    this.setState({shipS:"unselected"})
+    this.setState({questionS: "unselected"})
   }
   clickShip(e){
     this.setState({ship: "show"})
     this.setState({detail: "hide"})
     this.setState({question: "hide"})
+    this.setState({shipS: "selected"})
+    this.setState({detailS:"unselected"})
+    this.setState({questionS: "unselected"})
   }
   clickQuestion(e){
     this.setState({question: "show"})
     this.setState({detail: "hide"})
     this.setState({ship: "hide"})
+    this.setState({questionS: "selected"})
+    this.setState({detailS:"unselected"})
+    this.setState({shipS: "unselected"})
   }
 
   getOne(e){
@@ -59,11 +71,11 @@ export default class App extends Component {
         <h2>About this item </h2>
         <div className={"product-tabs"}>
           <div className={"tabs"}>
-            <div className={"tab"} onClick={this.clickDetail.bind(this)}>Details</div>
-            <div className={"tab"} onClick={this.clickShip.bind(this)}>Shipping and Returns</div>
-            <div className={"tab"}  onClick={this.clickQuestion.bind(this)}>Q&A</div>
+            <button type={"button"} className={this.state.detailS} onClick={this.clickDetail.bind(this)}><div className={"tab"} >Details</div> </button>
+            <button type={"button"}  className={this.state.shipS} onClick={this.clickShip.bind(this)}><div className={"tab"} >Shipping and Returns</div></button>
+            <button  type={"button"} className={this.state.questionS} onClick={this.clickQuestion.bind(this)}><div className={"tab"}  >Q&A</div></button>
           </div>
-          <hr/>
+         <hr/>
          <Detail  class={this.state.detail} product={this.state.prodInfo} />
          <Shipping  class={this.state.ship} product={this.state.prodInfo}/>
          <Question  class={this.state.question} product={this.state.prodInfo}/>
