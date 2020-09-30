0e0742c06c1d42d3b7006c9d698e1125858647a1
john <johnferguson1440@yahoo.com>
2020-09-30 08:30:22 -0500

    testing

--- frontend/src/Tabs.js
+++ frontend/src/Tabs.js
@@ -4,7 +4,12 @@ export default class App extends Component {
     constructor(props) {
       super(props);    
       this.state = { 
-            
+            prodName: "",
+            size: "",
+            material: "",
+            features: "",
+            cleaning: "",
+            description: ""
         
         };     
     }
